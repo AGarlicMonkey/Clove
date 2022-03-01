f550d20050f82d0ec3585e8d9055a05f01ca0cae
Alex <wolfplayeral@yahoo.co.uk>
2022-03-01 20:08:36 +0000

    `EditorVFS::resolve` properly resolves directories

--- bulb/components/membrane/source/EditorVFS.cpp
+++ bulb/components/membrane/source/EditorVFS.cpp
@@ -13,24 +13,29 @@ namespace membrane {
         std::filesystem::path assetPath{ contentDirectory / path };
         std::filesystem::path systemPath{};
 
-        //TEMP: Special case for scene files
-        //TODO: Treat scenes as asset files as well? Use ["asset"]["type"] to differentiate.
-        if(assetPath.extension() == ".clvscene") {
-            return assetPath.lexically_normal();
-        }
+        if(!std::filesystem::is_directory(assetPath)) {
+            //TEMP: Special case for scene files
+            //TODO: Treat scenes as asset files as well? Use ["asset"]["type"] to differentiate.
+            if(assetPath.extension() == ".clvscene") {
+                return assetPath.lexically_normal();
+            }
 
-        assetPath.replace_extension(".clvasset");
+            assetPath.replace_extension(".clvasset");
 
-        if(std::filesystem::exists(assetPath)) {
-            serialiser::Node assetNode{ loadYaml(assetPath).getValue() };
+            if(std::filesystem::exists(assetPath)) {
+                serialiser::Node assetNode{ loadYaml(assetPath).getValue() };
 
-            std::filesystem::path const relativePath{ assetNode["asset"]["path"].as<std::string>() };
-            systemPath = contentDirectory / relativePath;
+                std::filesystem::path const relativePath{ assetNode["asset"]["path"].as<std::string>() };
+                systemPath = contentDirectory / relativePath;
 
-            CLOVE_LOG(MembraneEditorVFS, LogLevel::Trace, "Resolved {0} into {1}", path.string(), systemPath.lexically_normal().string());
+                CLOVE_LOG(MembraneEditorVFS, LogLevel::Trace, "Resolved {0} into {1}", path.string(), systemPath.lexically_normal().string());
+            } else {
+                CLOVE_LOG(MembraneEditorVFS, LogLevel::Error, "Could not find asset file {0}", assetPath.string());
+                throw std::runtime_error("Could not load asset file. See log");
+            }
         } else {
-            CLOVE_LOG(MembraneEditorVFS, LogLevel::Error, "Could not find asset file {0}", assetPath.string());
-            throw std::runtime_error("Could not load asset file. See log");
+            //The path being resolved is a directory then resolve it normally.
+            systemPath = assetPath;
         }
 
         return systemPath.lexically_normal();
