724415f7403614a8ddff57b8895e44fda77be50d
Alex <wolfplayeral@yahoo.co.uk>
2022-03-14 21:42:43 +0000

    Implemented the drag drop visuals for the directory tree item

--- bulb/source/Views/FileExplorer/DirectoryTreeItem.xaml.cs
+++ bulb/source/Views/FileExplorer/DirectoryTreeItem.xaml.cs
@@ -7,6 +7,7 @@ namespace Bulb {
     /// An idividual item inside of a directory tree.
     /// </summary>
     public partial class DirectoryTreeItem : UserControl {
+        private FolderViewModel ViewModel => DataContext as FolderViewModel;
         private bool isExpanded = false;
 
         public DirectoryTreeItem() {
@@ -20,10 +21,10 @@ namespace Bulb {
             isExpanded = !isExpanded;
 
             if (isExpanded) {
-                var viewModel = (FolderViewModel)DataContext;
-                foreach (var subDir in viewModel.SubDirectories) {
-                    var item = new DirectoryTreeItem();
-                    item.DataContext = subDir;
+                foreach (var subDir in ViewModel.SubDirectories) {
+                    var item = new DirectoryTreeItem {
+                        DataContext = subDir
+                    };
 
                     ContentPanel.Children.Add(item);
                 }
@@ -36,6 +37,52 @@ namespace Bulb {
 
         private void UpdateExpansionIcon() {
             ExpandIcon.Content = isExpanded ? "V" : ">";
+        }
+
+        private void Button_DragEnter(object sender, DragEventArgs e) {
+            DragDropBorder.BorderThickness = new Thickness(3);
+            DragDropBorder.Padding = new Thickness(0);
+            e.Handled = true;
+        }
+
+        private void Button_DragLeave(object sender, DragEventArgs e) {
+            DragDropBorder.BorderThickness = new Thickness(0);
+            DragDropBorder.Padding = new Thickness(3);
+            e.Handled = true;
+        }
+
+        private void Button_DragOver(object sender, DragEventArgs e) {
+            e.Effects = DragDropEffects.None;
+
+            if (CanDrop(e.Data)) {
+                e.Effects = DragDropEffects.Move;
+            }
+
+            e.Handled = true;
+        }
+
+        private void Button_Drop(object sender, DragEventArgs e) {
+            e.Effects = DragDropEffects.None;
+
+            if (CanDrop(e.Data)) {
+                var data = ((DragDropData)e.Data.GetData(typeof(DragDropData)));
+                ViewModel.OnFileDropped(data.assetViewModel);
+                e.Effects = DragDropEffects.Move;
+            }
+
+            DragDropBorder.BorderThickness = new Thickness(0);
+            DragDropBorder.Padding = new Thickness(3);
+
+            e.Handled = true;
+        }
+
+        private bool CanDrop(IDataObject dataObject) {
+            if (dataObject.GetDataPresent(typeof(DragDropData))) {
+                var data = ((DragDropData)dataObject.GetData(typeof(DragDropData)));
+                return data.assetViewModel != ViewModel && ViewModel.CanDropFile(data.assetViewModel.FullPath);
+            } else {
+                return false;
+            }
         }
     }
 }
