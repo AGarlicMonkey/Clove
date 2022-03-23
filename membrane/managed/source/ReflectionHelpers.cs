a2c632e9470057e75a4542b4c95c254a076fb12b
Alex <wolfplayeral@yahoo.co.uk>
2022-03-23 07:53:36 +0000

    Fixed namespace

--- membrane/managed/source/ReflectionHelpers.cs
+++ membrane/managed/source/ReflectionHelpers.cs
@@ -2,7 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Runtime.InteropServices;
 
-namespace Bulb {
+namespace Membrane {
     [StructLayout(LayoutKind.Sequential)]
     public struct TypeInfo {
         string typeName;
