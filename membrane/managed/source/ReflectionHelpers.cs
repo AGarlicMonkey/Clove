a3ec0deeb4bce9bd44ff6d36ca7368053115b6da
Alex <wolfplayeral@yahoo.co.uk>
2022-03-23 08:03:11 +0000

    Remove try / catch from ReflectionHelpers
    If anything goes wrong with the pinvoke this is a developer issue which should be fixed

--- membrane/managed/source/ReflectionHelpers.cs
+++ membrane/managed/source/ReflectionHelpers.cs
@@ -11,41 +11,21 @@ namespace Membrane {
 
     public class ReflectionHelpers {
         public static List<TypeInfo> GetEditorVisibleComponents() {
-            List<TypeInfo> visibleComponents;
+            uint numInfos = 0;
+            getEditorVisibleComponents(null, ref numInfos);
+            TypeInfo[] infos = new TypeInfo[numInfos];
+            getEditorVisibleComponents(infos, ref numInfos);
 
-            try {
-                uint numInfos = 0;
-                getEditorVisibleComponents(null, ref numInfos);
-                TypeInfo[] infos = new TypeInfo[numInfos];
-                getEditorVisibleComponents(infos, ref numInfos);
-
-                visibleComponents = new List<TypeInfo>(infos);
-            } catch(Exception e) {
-                //TODO: Log
-
-                visibleComponents = new List<TypeInfo>();
-            }
-
-            return visibleComponents;
+            return new List<TypeInfo>(infos);
         }
 
         public static List<TypeInfo> GetEditorVisibleSubSystems() {
-            List<TypeInfo> visibleSubSystems;
+            uint numInfos = 0;
+            getEditorVisibleSubSystems(null, ref numInfos);
+            TypeInfo[] infos = new TypeInfo[numInfos];
+            getEditorVisibleSubSystems(infos, ref numInfos);
 
-            try {
-                uint numInfos = 0;
-                getEditorVisibleSubSystems(null, ref numInfos);
-                TypeInfo[] infos = new TypeInfo[numInfos];
-                getEditorVisibleSubSystems(infos, ref numInfos);
-
-                visibleSubSystems = new List<TypeInfo>(infos);
-            } catch(Exception e) {
-                //TODO: Log
-
-                visibleSubSystems = new List<TypeInfo>();
-            }
-
-            return visibleSubSystems;
+            return new List<TypeInfo>(infos);
         }
 
         [DllImport("MembraneNative.dll")]
