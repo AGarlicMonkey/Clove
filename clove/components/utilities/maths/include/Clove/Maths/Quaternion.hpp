5537fb58a9ba06b01f65f3263a60da8f701e2279
Alex <wolfplayeral@yahoo.co.uk>
2022-03-29 21:30:41 +0100

    Added docs for Quaternions

--- clove/components/utilities/maths/include/Clove/Maths/Quaternion.hpp
+++ clove/components/utilities/maths/include/Clove/Maths/Quaternion.hpp
@@ -45,18 +45,53 @@ namespace clove {
 
 //Quaternion operations
 namespace clove {
+    /**
+     * @brief Returns the length of a quaternion.
+     * @tparam T The type of the quaternion.
+     * @param q 
+     * @return 
+     */
     template<number T>
     constexpr T length(quat<T> const &q);
 
+    /**
+     * @brief Returns a normalised quaternion.
+     * @tparam T The type of the quaternion.
+     * @param q 
+     * @return 
+     */
     template<number T>
     constexpr quat<T> normalise(quat<T> const &q);
 
+    /**
+     * @brief Returns the dot product of two quaternions.
+     * @tparam T The type of each quaternion.
+     * @param a 
+     * @param b 
+     * @return 
+     */
     template<number T>
     constexpr T dot(quat<T> const &a, quat<T> const &b);
 
+    /**
+     * @brief Performs a linear interpolation between a and b by t.
+     * @tparam T The type of each quaternion.
+     * @param a 
+     * @param b 
+     * @param t A value between 0.0f and 1.0f.
+     * @return 
+     */
     template<number T>
     constexpr quat<T> lerp(quat<T> const &a, quat<T> const &b, float const t);
 
+    /**
+     * @brief Performs a spherical linear interpolation between a and b by t.
+     * @tparam T The type of each quaternion.
+     * @param a 
+     * @param b 
+     * @param t A value between 0.0f and 1.0f.
+     * @return 
+     */
     template<number T>
     constexpr quat<T> slerp(quat<T> const &a, quat<T> const &b, float const t);
 }
